a721d70162add78a4d1efcd89a3abc52f0081632
Davi Martins <davi_pmp@yahoo.com.br>
2023-02-25 18:52:27 -0300

    retirei as linhas adicionadas

--- Jogador1/src/TesteJogador.java
+++ Jogador1/src/TesteJogador.java
@@ -7,8 +7,5 @@ public class TesteJogador {
 		JogadorUm j1 = new JogadorUm();
 		
 		System.out.println(j1.isEnvenenado());
-		
-		System.out.println("Davi GitHub");
-		System.out.println("valeu big");
 	}
 }
