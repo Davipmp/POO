aa607ba8f2ee811e9bb1f08b7e5c24c7c06fdda0
Davi Martins <davi_pmp@yahoo.com.br>
2023-02-28 12:02:50 -0300

    Criacao de uma loja virtual, trazendo informacoes como marca, modelo, valor e desconto de 10% na primeira compra.

---
+++ Jogador1/src/loja_virtual/DescontoPrimeiraCompra.java
@@ -0,0 +1,22 @@
+package loja_virtual;
+
+public class DescontoPrimeiraCompra extends LojaVirtual {
+	
+	private double desconto;
+	
+
+	public double CalcularDesconto( String marca, String modelo, double desconto) {
+		this.desconto = valor;
+		return valor; 
+	}
+	
+	public double calculaDesconto() {
+		this.desconto = valor * desconto;
+		return valor; 
+	}
+
+	public double getDesconto() {
+		this.desconto = valor * 0.1;
+		return desconto;
+	}
+}
